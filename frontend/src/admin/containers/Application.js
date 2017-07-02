44d9c71ff07efdf7c3bceb1108a4c144bc8fc872
Kai Davenport <kaiyadavenport@gmail.com>
2017-07-02 19:27:38 +0100

    overall application layout

--- frontend/src/admin/containers/Application.js
+++ frontend/src/admin/containers/Application.js
@@ -7,46 +7,13 @@ import {
   valueSelectors
 } from '../selectors'
 
-import Application from 'template-ui/lib/components/layout/Application'
-import AppBar from '../components/layout/AppBar'
-import GuestMenu from '../components/layout/GuestMenu'
-import UserMenu from '../components/layout/UserMenu'
+import Application from '../components/layout/Application'
 
 class ApplicationContainer extends Component {
   render() {
-
-    const appbar = (
-      <AppBar 
-        toggleMenu={ this.props.toggleMenu }
-      />
-    )
-
-    const menu = this.props.user ?
-      (
-        <UserMenu
-          onClick={ this.props.onMenuClick }
-        />
-      ) :
-      (
-        <GuestMenu
-          onClick={ this.props.onMenuClick }
-        />
-      )
-
-    console.log('-------------------------------------------');
-    console.dir(this.props)
-
     return (
-      <Application
-        menu={ menu }
-        appbar={ appbar }
-        menuOpen={ this.props.menuOpen }
-        toggleMenu={ this.props.toggleMenu }
-      >
-        { this.props.children }
-      </Application>
+      <Application {...this.props} />
     )
-    
   }
 }
 
