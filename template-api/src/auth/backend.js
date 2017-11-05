74ceecc786349ceb266f5444f66f219526fe7d2a
Kai Davenport <kaiyadavenport@gmail.com>
2017-11-05 16:02:59 +0000

    passport with external providers

--- template-api/src/auth/backend.js
+++ template-api/src/auth/backend.js
@@ -183,8 +183,6 @@ const AuthBackend = (opts) => {
       })
     }
   })
-  
-
 
   /*
   
--- template-api/src/auth/passport.js
+++ template-api/src/auth/passport.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const async = require('async')
 const options = require('template-tools/src/utils/options')
 const passport = require('passport')
 
@@ -13,6 +14,38 @@ const Passport = (opts) => {
   })
 
   const authClient = opts.authClient  
+  const providers = opts.providers || []
+
+  providers.forEach(provider => {
+    const strategy = new provider.strategy(provider.opts, (token, tokenSecret, profile, done) => {
+      async.waterfall([
+        (next) => {
+          authClient.ensure({
+            username: profile.id,
+            password: profile.id
+          }, next)
+        },
+
+        (user, next) => {
+
+          const data = user.meta || {}
+          
+          data[provider.id] = profile
+          data[provider.id + 'Token'] = {
+            value: token,
+            secret: tokenSecret
+          }
+
+          authClient.update({
+            id: user.id,
+            data
+          }, next)
+        }
+      ], done)
+  
+    })
+    passport.use(strategy)
+  })
 
   passport.serializeUser((req, user, done) => {
     const id = opts.extractUserId ?
